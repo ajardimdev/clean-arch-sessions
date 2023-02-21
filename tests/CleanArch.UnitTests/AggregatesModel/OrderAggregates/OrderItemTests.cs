1d322150c086ba150195ad81685da75f4aa606db
drt.ajardim@ifood3rd.com <drt.ajardim@ifood3rd.com>
2023-02-21 15:46:15 -0300

    feat: OrderItem validation tests

--- tests/CleanArch.UnitTests/AggregatesModel/OrderAggregates/OrderItemTests.cs
+++ tests/CleanArch.UnitTests/AggregatesModel/OrderAggregates/OrderItemTests.cs
@@ -108,4 +108,96 @@ public class OrderItemTests
         // Then
         item.Cost.Should().Be(price * newQuantity);
     }
+
+    [Fact]
+    public void Should_OrderItem_Validate_Quantity_Must_Be_Greater_Than_0()
+    {
+        // Given
+        Faker faker = new("en");
+        ProductValidator productValidator = new();
+        OrderItemValidator orderItemValidator = new(productValidator);
+
+        var name = faker.Commerce.ProductName();
+        var price = faker.Random.Decimal(10m, 100m);
+        Product product =  Product.NewProduct(name, price);
+
+        var quantity = faker.Random.Int(-10, -1);
+        OrderItem item = OrderItem.NewOrderItem(product, quantity);
+    
+        // When
+        var validationResults = orderItemValidator.Validate(item);
+
+        // Then
+        validationResults.Errors.Should().Contain(_ => 
+            _.ErrorMessage == "'Quantity' must be greater than '0'.");
+    }
+
+    [Fact]
+    public void Should_OrderItem_Validate_Cost_Must_Be_Greater_Than_0()
+    {
+        // Given
+        Faker faker = new("en");
+        ProductValidator productValidator = new();
+        OrderItemValidator orderItemValidator = new(productValidator);
+
+        var name = faker.Commerce.ProductName();
+        var price = faker.Random.Decimal(-100m, -10m);
+        Product product =  Product.NewProduct(name, price);
+
+        var quantity = faker.Random.Int(1, 10);
+        OrderItem item = OrderItem.NewOrderItem(product, quantity);
+    
+        // When
+        var validationResults = orderItemValidator.Validate(item);
+
+        // Then
+        validationResults.Errors.Should().Contain(_ => 
+            _.ErrorMessage == "'Cost' must be greater than '0'.");
+    }
+
+    [Fact]
+    public void Should_OrderItem_Validate_Product_Name_Must_Not_Be_Empty()
+    {
+        // Given
+        Faker faker = new("en");
+        ProductValidator productValidator = new();
+        OrderItemValidator orderItemValidator = new(productValidator);
+
+        var name = string.Empty;
+        var price = faker.Random.Decimal(10m, 100m);
+        Product product =  Product.NewProduct(name, price);
+
+        var quantity = faker.Random.Int(1, 10);
+        OrderItem item = OrderItem.NewOrderItem(product, quantity);
+    
+        // When
+        var validationResults = orderItemValidator.Validate(item);
+
+        // Then
+        validationResults.Errors.Should().Contain(_ => 
+            _.ErrorMessage == "'Name' must not be empty.");
+    }
+
+    [Fact]
+    public void Should_OrderItem_Validate_Product_Price_Must_Be_Greater_Than_0()
+    {
+        // Given
+        Faker faker = new("en");
+        ProductValidator productValidator = new();
+        OrderItemValidator orderItemValidator = new(productValidator);
+
+        var name = faker.Commerce.ProductName();
+        var price = faker.Random.Decimal(-100m, -10m);
+        Product product =  Product.NewProduct(name, price);
+
+        var quantity = faker.Random.Int(1, 10);
+        OrderItem item = OrderItem.NewOrderItem(product, quantity);
+    
+        // When
+        var validationResults = orderItemValidator.Validate(item);
+
+        // Then
+        validationResults.Errors.Should().Contain(_ => 
+            _.ErrorMessage == "'Price' must be greater than '0'.");
+    }
 }
